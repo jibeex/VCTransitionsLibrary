e4c5654073d2f88e251b3088dcb26faf2137d9d6
MarkeJave <308865427@qq.com>
2017-10-15 19:12:02 +0800

    Added support for carthage.

--- AnimationControllers/CEReversibleAnimationController.h
+++ AnimationControllers/CEReversibleAnimationController.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 /**
  A base class for animation controllers which provide reversible animations. A reversible animation is often used with navigation controllers where the reverse property is set based on whether this is a push or pop operation, or for modal view controllers where the reverse property is set based o whether this is a show / dismiss.
---
+++ Framework/VCTransitionsLibrary/VCTransitionsLibrary.h
@@ -0,0 +1,34 @@
+//
+//  VCTransitionsLibrary.h
+//  VCTransitionsLibrary
+//
+//  Created by xulinfeng on 2017/10/15.
+//  Copyright © 2017年 xulinfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VCTransitionsLibrary.
+FOUNDATION_EXPORT double VCTransitionsLibraryVersionNumber;
+
+//! Project version string for VCTransitionsLibrary.
+FOUNDATION_EXPORT const unsigned char VCTransitionsLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCTransitionsLibrary/PublicHeader.h>
+
+#import<VCTransitionsLibrary/CEBaseInteractionController.h>
+#import<VCTransitionsLibrary/CEHorizontalSwipeInteractionController.h>
+#import<VCTransitionsLibrary/CEVerticalSwipeInteractionController.h>
+#import<VCTransitionsLibrary/CEPinchInteractionController.h>
+
+#import<VCTransitionsLibrary/CEReversibleAnimationController.h
+#import<VCTransitionsLibrary/CECubeAnimationController.h>
+#import<VCTransitionsLibrary/CEPanAnimationController.h>
+#import<VCTransitionsLibrary/CEFoldAnimationController.h>
+#import<VCTransitionsLibrary/CEFlipAnimationController.h>
+#import<VCTransitionsLibrary/CEPortalAnimationController.h>
+#import<VCTransitionsLibrary/CETurnAnimationController.h>
+#import<VCTransitionsLibrary/CECrossfadeAnimationController.h>
+#import<VCTransitionsLibrary/CENatGeoAnimationController.h>
+#import<VCTransitionsLibrary/CEExplodeAnimationController.h>
+#import<VCTransitionsLibrary/CECardsAnimationController.h>
